201148fdfb225310256215b54da72be43c5ef864
Y-CChen <yichern.chen@gmail.com>
2021-04-29 18:50:44 +0800

    use list

--- pages/liff/index.vue
+++ pages/liff/index.vue
@@ -1,12 +1,21 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
+    <v-col cols="12">
       <v-card>
         <v-card-title class="headline">
           LIFF
         </v-card-title>
-        <v-card-text>
-          <v-treeview v-if="logged" dense open-all :items="items" />
+        <v-card-text v-if="logged">
+          <v-list>
+            <v-list-item v-for="prop in props" :key="prop.name">
+              <v-list-item-content>
+                <v-list-item-title>{{ prop.name }}</v-list-item-title>
+                <v-list-item-subtitle class="text-wrap">
+                  <span>{{ prop.value }}</span>
+                </v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
         </v-card-text>
         <v-card-actions />
       </v-card>
@@ -22,11 +31,8 @@ const liffInit = liff.init({ liffId: process.env.liffId });
 export default {
   data() {
     return {
-      snackbar: false,
-      snackbarText: '',
       logged: false,
-      items: [],
-      test: false,
+      props: [],
     };
   },
   async mounted() {
@@ -41,33 +47,25 @@ export default {
         return await utils.makeForeverPromise();
       } else {
         this.logged = true;
-        this.items.push(this.makeItem('OS', liff.getOS()));
-        this.items.push(this.makeItem('language', liff.getLanguage()));
-        this.items.push(this.makeItem('version', liff.getVersion()));
-        this.items.push(this.makeItem('LINE version', liff.getLineVersion()));
-        this.items.push(
-          this.makeItem('running in LIFF browser', liff.isInClient()),
-        );
-        this.items.push(
-          this.makeItem('API available', {
+        this.props = [].concat(
+          this.makeProp('OS', liff.getOS()),
+          this.makeProp('language', liff.getLanguage()),
+          this.makeProp('version', liff.getVersion()),
+          this.makeProp('LINE version', liff.getLineVersion()),
+          this.makeProp('running in LIFF browser', liff.isInClient()),
+          this.makeProp('API available', {
             'share target picker': liff.isApiAvailable('shareTargetPicker'),
             'multiple liff transition': liff.isApiAvailable(
               'multipleLiffTransition',
             ),
           }),
-        );
-        this.items.push(this.makeItem('access token', liff.getAccessToken()));
-        this.items.push(this.makeItem('ID token', liff.getIDToken()));
-        this.items.push(
-          this.makeItem('decoded ID token', liff.getDecodedIDToken()),
-        );
-        this.items.push(this.makeItem('context', liff.getContext()));
-        this.items.push(this.makeItem('profile', await liff.getProfile()));
-        this.items.push(
-          this.makeItem('friendship', await liff.getFriendship()),
-        );
-        this.items.push(
-          this.makeItem('permanent link', liff.permanentLink.createUrl()),
+          this.makeProp('access token', liff.getAccessToken()),
+          this.makeProp('ID token', liff.getIDToken()),
+          this.makeProp('decoded ID token', liff.getDecodedIDToken()),
+          this.makeProp('context', liff.getContext()),
+          this.makeProp('profile', await liff.getProfile()),
+          this.makeProp('friendship', await liff.getFriendship()),
+          this.makeProp('permanent link', liff.permanentLink.createUrl()),
         );
       }
     } catch (e) {
@@ -75,16 +73,15 @@ export default {
     }
   },
   methods: {
-    makeItem(name, value) {
+    makeProp(name, value) {
       if (value !== null && typeof value === 'object') {
-        return {
-          name,
-          children: Object.keys(value).map((k) => {
-            return this.makeItem(k, value[k]);
-          }),
-        };
+        return Object.keys(value)
+          .map((k) => {
+            return this.makeProp(`${name}/${k}`, value[k]);
+          })
+          .reduce((previous, current) => previous.concat(current), []);
       } else {
-        return { name: `${name}: ${value}` };
+        return [{ name, value: `${value}` }];
       }
     },
   },
