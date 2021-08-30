b99cfeee332ceed2f3ac09199e4d3f446a3f4e7a
Y-CChen <yichern.chen@gmail.com>
2021-08-30 18:13:42 +0800

    header

--- pages/index.vue
+++ pages/index.vue
@@ -1,110 +1,43 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-title class="headline">
-          {{
-            `Current Time ${$moment
-              .utc()
-              .local()
-              .format('YYYY-MM-DD HH:mm:ss')}`
-          }}
-        </v-card-title>
-        <v-card-title class="headline">
-          {{ `Backend URL ${$nuxt.context.env.backendUrl}` }}
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>
-            .
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>
-            .
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>
-            .
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br />
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt :to="localePath('/inspire')">
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <v-img :src="`${$nuxt.context.env.baseUrl}/header.svg`" width="100%" light>
+    <v-container fluid="true" style="width: 100%; height: 100%;">
+      <v-row no-gutters style="height: 100%;" align="end" justify="end">
+        <v-col cols="auto">
+          <v-row no-gutters>
+            <template v-for="(l, i) in links">
+              <v-col
+                v-if="l.text"
+                :key="l.text"
+                class="d-flex align-center"
+                cols="auto"
+              >
+                <v-icon v-if="l.icon">
+                  {{ l.icon }}
+                </v-icon>
+                {{ l.text }}
+              </v-col>
+              <v-col :key="i" cols="auto">
+                <v-divider class="mx-4" vertical />
+              </v-col>
+            </template>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-img>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue';
-import VuetifyLogo from '~/components/VuetifyLogo.vue';
-
 export default {
-  middleware: ['authenticated'],
-  components: {
-    Logo,
-    VuetifyLogo,
-  },
+  data: () => ({
+    links: [
+      {},
+      { text: '優選優勢' },
+      { text: '團主口碑推薦' },
+      { text: '火熱商品開團中' },
+      { text: '快速加入' },
+      { text: '關於我們', icon: 'mdi-account-outline' },
+    ],
+  }),
 };
 </script>
