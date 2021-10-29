88645cbb059639e5d41da3e99571d373e00f2490
Y-CChen <yichern.chen@gmail.com>
2021-10-29 15:45:15 +0800

    fake-mfme-map

---
+++ pages/fake-mfme-map.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-card flat>
+    <v-card-title>{{ $route.query.callback }}</v-card-title>
+    <v-card-actions>
+      <v-btn outlined @click="goCallback()">
+        go
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  methods: {
+    goCallback() {
+      location.assign(this.$route.query.callback);
+    },
+  },
+};
+</script>
