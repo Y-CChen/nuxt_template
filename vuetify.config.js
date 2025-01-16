ab44bf3d0576c5a4b59a82150f59ecf969dad5ca
Y-CChen <250f42mt@duck.com>
2025-01-16 16:12:53 +0800

    enhance Vuetify theme configuration with custom properties and light theme colors

--- vuetify.config.js
+++ vuetify.config.js
@@ -15,7 +15,37 @@ export default function (context) {
       },
     },
     theme: {
-      customProperties: true,
+      options: {
+        customProperties: true,
+      },
+      themes: {
+        light: {
+          primary: {
+            lighten5: '#f1f0f7',
+            lighten4: '#e5e3f1',
+            lighten3: '#d2cce6',
+            lighten2: '#b9afd6',
+            lighten1: '#9e8ec4',
+            base: '#9079b6',
+            darken1: '#7b61a0',
+            darken2: '#665186',
+            darken3: '#54446e',
+            darken4: '#362b4a',
+          },
+          secondary: {
+            lighten5: '#f2f9f8',
+            lighten4: '#ddf0ef',
+            lighten3: '#bee3e3',
+            lighten2: '#92cdce',
+            lighten1: '#67b5b7',
+            base: '#429498',
+            darken1: '#3a7b80',
+            darken2: '#34646a',
+            darken3: '#315359',
+            darken4: '#2c484d',
+          },
+        },
+      },
     },
   };
 }
