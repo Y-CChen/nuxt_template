bda78569b617ad37207e26bc072efd53002c9570
Y-CChen <250f42mt@duck.com>
2025-04-07 15:00:07 +0800

    refactor logout functionality to integrate auth middleware and improve logout process

--- nuxt.config.js
+++ nuxt.config.js
@@ -109,7 +109,7 @@ export default {
     plugins: ['~/plugins/auth.js'],
     redirect: {
       login: '/login',
-      logout: '/',
+      logout: false,
       callback: '/login',
       home: '/',
     },
--- plugins/auth.js
+++ plugins/auth.js
@@ -1,1 +1,11 @@
-export default function () {}
+import { authMiddleware } from '@nuxtjs/auth-next/dist/runtime';
+
+export default function ({ app, $auth }) {
+  const logoutFunc = $auth.logout.bind($auth);
+  const logout = async (...args) => {
+    const result = await logoutFunc?.(...args);
+    authMiddleware(app.context);
+    return result;
+  };
+  $auth.logout = logout;
+}
