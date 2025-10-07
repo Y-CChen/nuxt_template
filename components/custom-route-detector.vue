e6f3769d7401a2c3def206cf65ead8821100cce6
Y-CChen <250f42mt@duck.com>
2025-10-07 18:41:40 +0800

    feat: add custom route detector component for dynamic navigation

---
+++ components/custom-route-detector.vue
@@ -0,0 +1,44 @@
+<template>
+  <nuxt-link
+    v-if="to"
+    v-slot="{ href, isActive, isExactActive, navigate, route }"
+    custom
+    :to="to"
+  >
+    <slot
+      name="default"
+      v-bind="{
+        href,
+        isActive: exact ? isExactActive : isActive,
+        navigate,
+        route,
+      }"
+    />
+  </nuxt-link>
+  <div v-else>
+    <slot
+      name="default"
+      v-bind="{
+        href: undefined,
+        isActive: false,
+        navigate: () => {},
+        route: undefined,
+      }"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    exact: {
+      type: Boolean,
+      default: false,
+    },
+    to: {
+      type: [Object, String],
+      default: undefined,
+    },
+  },
+};
+</script>
