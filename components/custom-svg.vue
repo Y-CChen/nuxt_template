5af5db795e4283f654bb052164d60f9f551675ee
Y-CChen <250f42mt@duck.com>
2025-12-22 18:50:31 +0800

    feat: add custom-svg component with template, script, and styles

---
+++ components/custom-svg.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg
+    class="custom-svg"
+    fill="none"
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:svg="http://www.w3.org/2000/svg"
+    v-bind="$attrs"
+    v-on="$listeners"
+  >
+    <slot name="default" />
+  </svg>
+</template>
+
+<script>
+export default {};
+</script>
