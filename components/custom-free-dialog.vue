817ae33514e883f8603dc4e54d45e094a1450a75
Y-CChen <250f42mt@duck.com>
2025-12-30 17:23:45 +0800

    feat: add custom-fill-height class and custom-free-dialog component for improved layout

---
+++ components/custom-free-dialog.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-dialog
+    v-model="computedValue"
+    class="fill-height"
+    content-class="elevation-0 rounded-0 ma-0 custom-fill-height"
+    width="100%"
+    :transition="transition"
+  >
+    <div
+      class="fill-height"
+      :class="contentClass"
+      @click="$emit('click:outside', $event)"
+    >
+      <slot name="default" />
+    </div>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      default: undefined,
+      validator() {
+        return true;
+      },
+    },
+    contentClass: {
+      type: [Object, String],
+      default: undefined,
+    },
+    transition: {
+      type: [Boolean, String],
+      default: undefined,
+    },
+  },
+  computed: {
+    computedValue: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit('input', value);
+      },
+    },
+  },
+};
+</script>
