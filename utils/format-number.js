8d8a5b79a9e5e699f448ea77bd135f6d6a534edd
Y-CChen <250f42mt@duck.com>
2026-01-05 14:21:40 +0800

    fix: handle NaN values in makeFormatNumber utility

--- utils/format-number.js
+++ utils/format-number.js
@@ -1,5 +1,9 @@
 export function makeFormatNumber(fractionDigits, roundingMode = 'halfExpand') {
   return (number) => {
+    number = parseFloat(number);
+    if (isNaN(number)) {
+      return '';
+    }
     return `${(number || 0).toLocaleString('en', {
       minimumFractionDigits: fractionDigits,
       maximumFractionDigits: fractionDigits,
