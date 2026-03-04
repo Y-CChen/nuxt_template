761718081ef70be00e67ee72b81afe9811add153
Y-CChen <250f42mt@duck.com>
2026-03-04 16:30:04 +0800

    feat: extend makeAbsolutePosition to support additional coordinates for positioning

--- utils/style.js
+++ utils/style.js
@@ -15,18 +15,26 @@ export function makeFixedHeightWidth({ height, width } = {}) {
   };
 }
 
-export function makeAbsolutePosition({ y, x, height, width } = {}) {
+export function makeAbsolutePosition({ y, x, y2, x2, height, width } = {}) {
   if (y !== undefined) {
     y = typeof y === 'number' ? `${y}px` : y;
   }
   if (x !== undefined) {
     x = typeof x === 'number' ? `${x}px` : x;
   }
+  if (y2 !== undefined) {
+    y2 = typeof y2 === 'number' ? `${y2}px` : y2;
+  }
+  if (x2 !== undefined) {
+    x2 = typeof x2 === 'number' ? `${x2}px` : x2;
+  }
   const fixedHeighWidth = makeFixedHeightWidth({ height, width });
   return {
     position: 'absolute',
     top: y,
     left: x,
+    bottom: y2,
+    right: x2,
     ...fixedHeighWidth,
   };
 }
