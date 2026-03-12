1192a2732664ed22948684327b074717b2f5d572
Y-CChen <250f42mt@duck.com>
2026-03-12 16:59:21 +0800

    feat: add custom scrollbar component with simplebar integration and update dependencies

---
+++ components/custom-scrollbar.vue
@@ -0,0 +1,26 @@
+<!-- <cSpell: ignore simplebar -->
+<template>
+  <simplebar
+    :data-simplebar-auto-hide="autoHide ? 'true' : 'false'"
+    @scroll="$emit('scroll', $event)"
+  >
+    <slot name="default" />
+  </simplebar>
+</template>
+
+<script>
+import simplebar from 'simplebar-vue';
+import 'simplebar-vue/dist/simplebar.min.css';
+
+export default {
+  components: {
+    simplebar,
+  },
+  props: {
+    autoHide: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
